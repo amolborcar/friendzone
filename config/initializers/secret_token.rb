3174d89827a7606941a88e66c77931c377aeb191
Amol Borcar <Amol@Amols-MacBook-Air.local>
2014-08-20 19:23:30 -0700

    Initial commit
    Initial commit with Rails 3.2.19 skeleton.  Changed database from sqlite3 to PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendzone::Application.config.secret_token = '8fc7c0d27280f5917d9d7fd81139d2421088480a279d95ed7c823bb73b1335cc0ca3f0c7bd79a6773478db5e91601b76d2d50f1c5e19f36f225465f8a9fcb034'
