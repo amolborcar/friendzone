dfa48c11f22f324d70358f8ecb5284966e50faea
Amol Borcar <aborcar@gmail.com>
2014-09-02 13:44:40 -0700

    App can connect to Facebook via Oauth but no functionality.
    App can connect to Facebook using the facebook_oauth gem.  However, break to add in analytics because not sure that Facebook data that I want is available via the facebook API.  Will look into this later.  Currently app is set up to integrate Facebook login later.
    
    Removing sign in with facebook button.

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -3,25 +3,39 @@ class NetworksController < ApplicationController
   def index
   end
 
-  def sign_in
-    session[:request_token] = Authorizer.request_token(session[:request_token], request.host)
-    redirect_to session[:request_token].authorize_url
-  end
-
   def sign_out
     TwitterLoader.clear_user_data
     session.clear
     redirect_to root_path
   end
 
-  def auth
+  def sign_in_twitter
+    session[:request_token] = Authorizer.request_token(session[:request_token], request.host)
+    redirect_to session[:request_token].authorize_url
+  end
+
+  def sign_in_facebook
+    facebook_client = Authorizer.create_facebook_client(request.host)
+    redirect_to Authorizer.authorize_facebook_request(facebook_client)
+    # redirect_to session[:facebook_request]
+  end
+
+
+  def auth_twitter
     @access_token = Authorizer.request_token(session[:request_token], request.host).get_access_token(:oauth_verifier => params[:oauth_verifier])
     session.delete(:request_token)
     Authorizer.save_token(@access_token)
-    redirect_to load_path
+    redirect_to load_twitter_path
   end
 
-  def load
+  def auth_facebook
+    client = Authorizer.create_facebook_client(request.host)
+    @facebook_token = client.authorize(:code => params[:code])
+    puts @facebook_token
+    puts client.me.home
+  end
+
+  def load_twitter
     fetcher = DataFetcher.new(Authorizer.instantiate_token)
     fetcher.get_twitter_data
     redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Friendzone::Application.routes.draw do
   get 'index', to: 'networks#index'
   post 'search', to: 'networks#search'
-  get 'sign_in', to: 'networks#sign_in'
+  get 'sign_in_twitter', to: 'networks#sign_in_twitter'
+  get 'sign_in_facebook', to: 'networks#sign_in_facebook'
   get 'sign_out', to: 'networks#sign_out'
-  get 'auth', to: 'networks#auth'
-  get 'load', to: 'networks#load'
+  get 'auth_twitter', to: 'networks#auth_twitter'
+  get 'load_twitter', to: 'networks#load_twitter'
+  get 'auth_facebook', to: 'networks#auth_facebook'
+  get 'load_facebook', to: 'networks#load_facebook'
   get 'find_all_names', to: 'networks#find_all_names'
   root to: 'networks#index'
 
